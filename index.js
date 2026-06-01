b172609537e5dd43915be16ab1cc0d5cb44d5776
karimbaidori10 <karimbaidori10@gmail.com>
2026-06-01 19:07:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ if (interaction.isChatInputCommand() && interaction.commandName === "removepoint
         content: `✅ ${points} Punkte von ${target.tag} entfernt.\n🏆 Neuer Stand: ${db[target.id]} Punkte`,
         ephemeral: true
     });
-}
+
 }// =====================// 📊 PANEL// =====================if (interaction.isChatInputCommand() && interaction.commandName === "panel") {
 
     const member = await interaction.guild.members.fetch(interaction.user.id);
